3c759f3164cf53072b4b04d3e7bbdffdce9e87e8
Albert <friendsnowy@gmail.com>
2020-10-01 03:05:02 +0500

    Checkboxes for settings controll

--- Config.lua
+++ Config.lua
@@ -5,9 +5,11 @@ SS_loadConfiguration = function()
     settings = {
       currentPlot = nil,
       selectedPlot = nil,
+      acceptInvites = true,
+      displayDiceInfo = false,
     },
   };
   if (SS_User.plots == nil) then SS_User.plots = { }; end;
   if (SS_User.leadingPlots == nil) then SS_User.leadingPlots = { }; end;
-  if (SS_User.settings == nil) then SS_User.settings = { currentPlot = nil, selectedPlot = nil }; end;
+  if (SS_User.settings == nil) then SS_User.settings = { currentPlot = nil, selectedPlot = nil, displayDiceInfo = false, acceptInvites = false }; end;
 end;
