38efa81b845277679807109c2ac6244d64e0d6ec
Albert <friendsnowy@gmail.com>
2020-11-09 02:05:51 +0500

    Armor view worked with frame template

--- Armor.lua
+++ Armor.lua
@@ -16,21 +16,21 @@ end;
 
 SS_Armor_DrawCheck = function()
   local armorType = SS_Plots_Current().armor;
-  SS_Armor_Menu_Armor_Light:SetChecked('false');
-  SS_Armor_Menu_Armor_Medium:SetChecked('false');
-  SS_Armor_Menu_Armor_Heavy:SetChecked('false');
+  SS_Armor_Menu_Light.check:SetChecked('false');
+  SS_Armor_Menu_Medium.check:SetChecked('false');
+  SS_Armor_Menu_Heavy.check:SetChecked('false');
   SS_Armor_Menu_Armor_Light_Visual:Hide();
   SS_Armor_Menu_Armor_Medium_Visual:Hide();
   SS_Armor_Menu_Armor_Heavy_Visual:Hide();
 
   if (armorType == 'light') then
-    SS_Armor_Menu_Armor_Light:SetChecked('true')
+    SS_Armor_Menu_Light.check:SetChecked('true')
     SS_Armor_Menu_Armor_Light_Visual:Show();
   elseif (armorType == 'medium') then
-    SS_Armor_Menu_Armor_Medium:SetChecked('true')
+    SS_Armor_Menu_Medium.check:SetChecked('true')
     SS_Armor_Menu_Armor_Medium_Visual:Show();
   elseif (armorType == 'heavy') then
-    SS_Armor_Menu_Armor_Heavy:SetChecked('true')
+    SS_Armor_Menu_Heavy.check:SetChecked('true')
     SS_Armor_Menu_Armor_Heavy_Visual:Show();
   end;
 end;
